9c203c94ee3fc3863cdedf43e4f721eae2d085c8
JerryKin <2181130@qq.com>
2017-07-30 21:14:24 +0800

    model 以及dto

---
+++ JCmsErp.Application/Users/UserInfoDto.cs
@@ -0,0 +1,37 @@
+﻿using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCmsErp.Users
+{
+    [Serializable]
+    [AutoMapFrom(typeof(Users))]
+    public class UserInfoDto
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string TrueName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public bool Enabled { get; set; }
+        public string StrEnabled
+        {
+            get
+            {
+                return Enabled == true ? "是" : "否";
+            }
+        }
+        public DateTime CreationTime { get; set; }
+
+    }
+}
---
+++ JCmsErp.Core/Users/Users.cs
@@ -0,0 +1,92 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCmsErp.Users
+{
+    [Table("Users")]
+    public class Users : Entity<int>, IHasCreationTime
+    {
+
+        /// <summary>
+        /// 登录名称
+        /// </summary>
+        [StringLength(20)]
+        public string UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required]
+        [StringLength(32)]
+        public string Password { get; set; }
+        /// <summary>
+        /// 真实姓名
+        /// </summary>
+        [Required]
+        [StringLength(20)]
+        public string TrueName { get; set; }
+
+        /// <summary>
+        /// 电子邮件
+        /// </summary>
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 是否激活
+        /// </summary>
+        public bool Enabled { get; set; }
+        public string StrEnabled
+        {
+            get
+            {
+                return Enabled == true ? "是" : "否";
+            }
+        }
+        public interface ICreationAudited : IHasCreationTime
+        {
+            /// <summary>
+            /// 创建人id
+            /// </summary>
+            long? CreatorUserId { get; set; }
+            /// <summary>
+            /// 最后更新时间
+            /// </summary>
+            DateTime? LastModificationTime { get; set; }
+        }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+        public Users()
+        {
+            CreationTime = DateTime.Now;
+        }
+        //软删除
+        public interface ISoftDelete
+        {
+            /// <summary>
+            /// 是否删除
+            /// </summary>
+            bool IsDeleted { get; set; }
+        }
+    }
+}
