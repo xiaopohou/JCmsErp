8b15a056907bf1cd4903945dd49959275a4c5e91
JerryKin <2181130@qq.com>
2017-07-31 23:19:20 +0800

    webapi

--- JCmsErp.WebApi/JCmsErpWebApiModule.cs
+++ JCmsErp.WebApi/JCmsErpWebApiModule.cs
@@ -3,6 +3,9 @@ using Abp.Application.Services;
 using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
+using Swashbuckle.Application;
+using System.Linq;
+
 
 namespace JCmsErp
 {
@@ -16,6 +19,21 @@ namespace JCmsErp
             Configuration.Modules.AbpWebApi().DynamicApiControllerBuilder
                 .ForAll<IApplicationService>(typeof(JCmsErpApplicationModule).Assembly, "app")
                 .Build();
+
+          //  Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+
+            ConfigureSwaggerUi();
+        }
+
+        private void ConfigureSwaggerUi()
+        {
+            Configuration.Modules.AbpWebApi().HttpConfiguration
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "YoYoCMS.PhoneBookAPI文档");
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                })
+                .EnableSwaggerUi();
         }
     }
 }
